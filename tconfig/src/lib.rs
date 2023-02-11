fdae8e41921a1824a93ddadf84a15b319efc1999
andre <12485443-Andre_HJR@users.noreply.gitlab.com>
2023-02-11 11:44:34 -0500

    tconfig attached

---
+++ tconfig/src/lib.rs
@@ -0,0 +1,24 @@
+#![no_std]
+#[allow(unused)]
+
+
+pub const USER_STACK_SIZE: usize = 4096 * 2;
+pub const KERNEL_STACK_SIZE: usize = 4096 * 2;
+pub const KERNEL_HEAP_SIZE: usize = 0x100_0000;
+pub const MEMORY_END: usize = 0x88000000;
+pub const PAGE_SIZE: usize = 0x1000;
+pub const PAGE_SIZE_BITS: usize = 0xc;
+
+pub const TRAMPOLINE: usize = usize::MAX - PAGE_SIZE + 1;
+pub const TRAP_CONTEXT_BASE: usize = TRAMPOLINE - PAGE_SIZE;
+
+// pub use drivers::board::{CLOCK_FREQ, MMIO};
+
+pub const CLOCK_FREQ: usize = 12500000;
+
+pub const MMIO: &[(usize, usize)] = &[
+    (0x0010_0000, 0x00_2000), // VIRT_TEST/RTC  in virt machine
+    (0x2000000, 0x10000),
+    (0xc000000, 0x210000), // VIRT_PLIC in virt machine
+    (0x10000000, 0x9000),  // VIRT_UART0 with GPU  in virt machine
+];
