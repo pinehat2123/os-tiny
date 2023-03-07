8d5ac5658d8d58bb6c6e4026e34bbbc4cd53dad1
andre <12485443-Andre_HJR@users.noreply.gitlab.com>
2023-03-07 10:54:31 -0500

    async_lib update

--- application/user/src/async_lib/mod.rs
+++ application/user/src/async_lib/mod.rs
@@ -32,6 +32,28 @@ pub fn execute_async_main(main: impl Future<Output = i32> + Send + Sync + 'stati
     unsafe { EXIT_CODE }
 }
 
+pub fn spawn(future: impl Future<Output = ()> + Send + Sync + 'static) {
+    let shared_payload = unsafe { task::shared::SharedPayload::new(SHARED_PAYLOAD_BASE) };
+    let asid = unsafe { task::shared::AddressSpaceId::from_raw(ADDRESS_SPACE_ID) };
+    let task = task::new_user(
+        future,
+        shared_payload.shared_scheduler,
+        shared_payload.shared_set_task_state,
+    );
+    unsafe {
+        shared_payload.add_task(0 /* todo */, asid, task.task_repr());
+    }
+}
+
+pub fn execute_async() {
+    let shared_payload = unsafe { task::shared::SharedPayload::new(SHARED_PAYLOAD_BASE) };
+    task::shared::run_until_ready(
+        || unsafe { shared_payload.peek_task(task::shared::user_should_switch) },
+        |task_repr| unsafe { shared_payload.delete_task(task_repr) },
+        |task_repr, new_state| unsafe { shared_payload.set_task_state(task_repr, new_state) },
+    );
+}
+
 use syscall::sys_yield;
 
 pub fn do_yield(next_asid: usize) -> usize {
