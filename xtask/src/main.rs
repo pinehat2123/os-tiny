6885d11e8c335e175abcecc6ed2496e2b6003099
andre <12485443-Andre_HJR@users.noreply.gitlab.com>
2023-03-20 17:01:17 -0400

    xtask reopen.

---
+++ xtask/src/main.rs
@@ -0,0 +1,99 @@
+use clap::{arg, Command};
+use std::path::PathBuf;
+
+fn cli() -> Command {
+    Command::new("xtask")
+        .about("xtask utils")
+        .subcommand_required(true)
+        .arg_required_else_help(true)
+        .allow_external_subcommands(true)
+        .subcommand(
+            // TODO: this is git
+            Command::new("git")
+                .about("A fictional versioning CLI")
+                .subcommand_required(true)
+                .arg_required_else_help(true)
+                .allow_external_subcommands(true)
+                .subcommand(
+                    Command::new("clone")
+                        .about("Clones repos")
+                        .arg(arg!(<REMOTE> "The remote to clone"))
+                        .arg_required_else_help(true),
+                )
+                .subcommand(
+                    Command::new("diff")
+                        .about("Compare two commits")
+                        .arg(arg!(base: [COMMIT]))
+                        .arg(arg!(head: [COMMIT]))
+                        .arg(arg!(path: [PATH]).last(true))
+                        .arg(
+                            arg!(--color <WHEN>)
+                                .value_parser(["always", "auto", "never"])
+                                .num_args(0..=1)
+                                .require_equals(true)
+                                .default_value("auto")
+                                .default_missing_value("always"),
+                        ),
+                )
+                .subcommand(
+                    Command::new("push")
+                        .about("pushes things")
+                        .arg(arg!(<REMOTE> "The remote to target"))
+                        .arg_required_else_help(true),
+                )
+                .subcommand(
+                    Command::new("add")
+                        .about("adds things")
+                        .arg_required_else_help(true)
+                        .arg(
+                            arg!(<PATH> ... "Stuff to add")
+                                .value_parser(clap::value_parser!(PathBuf)),
+                        ),
+                )
+                .subcommand(
+                    Command::new("stash")
+                        .args_conflicts_with_subcommands(true)
+                        .args(push_args())
+                        .subcommand(Command::new("push").args(push_args()))
+                        .subcommand(Command::new("pop").arg(arg!([STASH])))
+                        .subcommand(Command::new("apply").arg(arg!([STASH]))),
+                )
+                // TODO: This is cargo
+                .subcommand(
+                    Command::new("cargos")
+                        .about("Wrap cargo useage")
+                        .subcommand_required(true)
+                        .arg_required_else_help(true)
+                        .allow_external_subcommands(true),
+                )
+                // TODO: This is tiny
+                .subcommand(
+                    Command::new("tiny")
+                        .about("Deal With Tiny")
+                        .subcommand_required(true)
+                        .arg_required_else_help(true)
+                        .allow_external_subcommands(true),
+                ),
+        )
+}
+
+fn push_args() -> Vec<clap::Arg> {
+    vec![arg!(-m --message <MESSAGE>)]
+}
+
+fn main() {
+    let matches = cli().get_matches();
+    match matches.subcommand() {
+        Some(("git", sub_matches)) => match sub_matches.subcommand() {
+            Some(("clone", sub_matches)) => {
+                println!(
+                    "Clone {}",
+                    sub_matches.get_one::<String>("REMOTE").expect("required")
+                );
+            }
+            _ => {}
+        },
+        _ => {}
+    }
+}
+
