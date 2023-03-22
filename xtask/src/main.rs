2f13480f5a08ed6b1aa98e9c9eb4cacb60bfc522
andre <12485443-Andre_HJR@users.noreply.gitlab.com>
2023-03-22 12:14:05 -0400

    add cargo xtask tiny qemu -p riscv64.

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -1,4 +1,4 @@
-use clap::{arg, Command};
+use clap::{arg, Command, Arg};
 use std::path::PathBuf;
 
 fn cli() -> Command {
@@ -57,23 +57,52 @@ fn cli() -> Command {
                         .subcommand(Command::new("push").args(push_args()))
                         .subcommand(Command::new("pop").arg(arg!([STASH])))
                         .subcommand(Command::new("apply").arg(arg!([STASH]))),
-                )
-                // TODO: This is cargo
-                .subcommand(
-                    Command::new("cargos")
-                        .about("Wrap cargo useage")
-                        .subcommand_required(true)
-                        .arg_required_else_help(true)
-                        .allow_external_subcommands(true),
-                )
-                // TODO: This is tiny
-                .subcommand(
-                    Command::new("tiny")
-                        .about("Deal With Tiny")
-                        .subcommand_required(true)
-                        .arg_required_else_help(true)
-                        .allow_external_subcommands(true),
                 ),
+        )
+        // TODO: This is cargo
+        .subcommand(
+            Command::new("cargos")
+                .about("Wrap cargo useage")
+                .subcommand_required(true)
+                .arg_required_else_help(true)
+                .allow_external_subcommands(true),
+        )
+        // TODO: This is tiny
+        .subcommand(
+            Command::new("tiny")
+                .about("Deal With Tiny")
+                .subcommand_required(true)
+                .arg_required_else_help(true)
+                .allow_external_subcommands(true)
+                // For kernel compile
+                .subcommand(
+                    // CMD::new("/usr/bin/cargo").arg("tiny").arg("kernel").arg(format!("--target={}", <TARGET>)).arg(format!("--{}", <BUILDMODE>)).status().expect("tiny kernel build error.")
+                    Command::new("kernel")
+                        .about("This is the kernel compile requirement")
+                        .arg_required_else_help(true)
+                        .arg(arg!(<TARGET> "the target of kernel"))
+                        .arg(arg!(<BUILDMODE> "the build mode of kernel")),
+                )
+                // For ring_scheduler compile
+                .subcommand(
+                    Command::new("ring_scheduler")
+                        .about("Ring scheduler")
+                        .arg_required_else_help(true)
+                        .arg(arg!(<TARGET> "the target of ring_scheduler"))
+                        .arg(arg!(<BUILDMODLE> "the buiild mode of kernel")),
+                )
+                .subcommand(
+                    Command::new("qemu")
+                        .about("Qemu Run")
+                        .arg_required_else_help(true)
+                        .arg(
+                            Arg::new("plantform")
+                            .short('p')
+                            .help("plantform specify")
+                        )
+            )
+                // For env build
+                .subcommand(Command::new("env")),
         )
 }
 
@@ -81,6 +110,7 @@ fn push_args() -> Vec<clap::Arg> {
     vec![arg!(-m --message <MESSAGE>)]
 }
 
+use std::process::Command as CM;
 fn main() {
     let matches = cli().get_matches();
     match matches.subcommand() {
@@ -93,7 +123,21 @@ fn main() {
             }
             _ => {}
         },
+        Some(("tiny", sub_matches)) => match sub_matches.subcommand() {
+            Some(("qemu", sub_matches)) => {
+                if sub_matches.contains_id("plantform") {
+                    let plantform = sub_matches.get_one::<String>("plantform").expect("required");
+                    println!("Plantform {}", plantform);
+                    match &plantform as &str {
+                        "riscv64" => {
+                            CM::new("/usr/bin/make").arg("run").status().expect("Run tiny error");
+                        }
+                        _ => {}
+                    }
+                }
+            }
+            _ => {}
+        },
         _ => {}
     }
 }
-
