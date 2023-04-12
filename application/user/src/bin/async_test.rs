3c90955d3f1dfea38e4911d5b9f29ed22693641f
andre <12485443-Andre_HJR@users.noreply.gitlab.com>
2023-04-12 05:04:04 -0400

    user fib test.

--- application/user/src/bin/async_test.rs
+++ application/user/src/bin/async_test.rs
@@ -5,27 +5,64 @@ extern crate alloc;
 #[macro_use]
 extern crate user_lib;
 
-// use user_lib::async_lib::{do_yield, execute_async, read_timer, reset_timer, job_add};
-use user_lib::async_lib::task::wokes::Executor;
+use user_lib::{async_lib::task::wokes::Executor};
+use core::{future::Future, pin::Pin, task::{Context, Poll}};
 
+struct FibonacciFuture {
+    predecessor: usize,
+    successor: usize,
+    index: usize,
+    count: usize,
+}
+
+impl FibonacciFuture {
+    fn new (count: usize) -> FibonacciFuture {
+        FibonacciFuture { predecessor: 0, successor: 1, index: 0, count }
+    }
+}
+
+impl Future for FibonacciFuture {
+    type Output = usize;
+    fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        if self.index == self.count {
+            // println!("Fibonacci {} result: {}", self.count, self.predecessor);
+            Poll::Ready(self.predecessor)
+        } else {
+            let tmp = self.predecessor;
+            self.predecessor += self.successor;
+            self.successor = tmp;
+            self.index += 1;
+            println!("Fibonacci {}; index = {}, predecessor = {}, successor = {}", self.count, self.index, self.predecessor, self.successor);
+            cx.waker().wake_by_ref();
+            Poll::Pending
+        }
+    }
+}
 
 #[no_mangle]
 fn main() -> i32 {
-    // for i in 0..200 {
-    //     spawn(a(i))
-    // }
-    // job_add(a(0));
-    // reset_timer();
-    // execute_async();
-    // println!("[async test] coroutines timer: {}", read_timer());
-    // do_yield(2);
     let executor = Executor::default();
-    for _ in 1..20 {
-        executor.spawn(async {
-            println!("[userspace async] Hello world!")
-        });
-    }
-
+    // for i in (1..=50).rev() {
+    //     executor.spawn(async move {
+    //         let ans = FibonacciFuture::new(i).await;
+    //         println!("[user space] Fibonacci[{}] = {}", i, ans);
+    //     });
+    // }
+    executor.spawn(async {
+        let i = 50;
+        let ans = FibonacciFuture::new(i).await;
+        println!("[user space] Fibonacci[{}] = {}", i, ans);
+    });
+    executor.spawn(async {
+        let i = 11;
+        let ans = FibonacciFuture::new(i).await;
+        println!("[user space] Fibonacci[{}] = {}", i, ans);
+    });
+    executor.spawn(async {
+        let i = 1;
+        let ans = FibonacciFuture::new(i).await;
+        println!("[user space] Fibonacci[{}] = {}", i, ans);
+    });
     executor.run_until_idle();
     0
 }
