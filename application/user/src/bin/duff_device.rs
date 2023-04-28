6e5bd69293d0563d0e0f6ea06b276a611f4da2df
andre <12485443-Andre_HJR@users.noreply.gitlab.com>
2023-04-28 03:37:35 -0400

    move unsafe.

--- application/user/src/bin/duff_device.rs
+++ application/user/src/bin/duff_device.rs
@@ -9,23 +9,21 @@ const STATE_NUM: i32 = 68;
 
 #[no_mangle]
 pub fn main() -> i32 {
-    unsafe {
-        static mut STATE: i32 = 0;
-        static mut I: i32 = 0;
-        'outer: loop {
-            loop {
-                match STATE {
-                    0 => {
-                        STATE = 1;
-                        continue 'outer;
-                    }
-                    _ => {
-                        loop {
-                            STATE = 1; I += 1;
-                            println!("I_STATE: {}, STATE: {}", I, STATE);
-                            if I >= STATE_NUM { break 'outer; }
-                            else { continue 'outer; }
-                        }
+    let mut state: i32 = 0;
+    let mut i: i32 = 0;
+    'outer: loop {
+        loop {
+            match state {
+                0 => {
+                    state = 1;
+                    continue 'outer;
+                }
+                _ => {
+                    loop {
+                        state = 1; i += 1;
+                        println!("I_STATE: {}, STATE: {}", i, state);
+                        if i >= STATE_NUM { break 'outer; }
+                        else { continue 'outer; }
                     }
                 }
             }
